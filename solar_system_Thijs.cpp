9bf544f8d80ba3a605c6b806f867af5ffc97ef34
ThijsSpoor99 <tspoor@live.nl>
2025-01-12 13:34:04 +0100

    solar_system_Thijs now properly calculates the orbitals.

--- solar_system_Thijs.cpp
+++ solar_system_Thijs.cpp
@@ -20,6 +20,21 @@ namespace Constants {
     constexpr double G = 6.67430e-11;
 }
 
+// vector math (for loop is faster for small arrays)
+std::array<double, 3> subtract3D(const std::array<double, 3>& v1, const std::array<double, 3>& v2) {
+    std::array<double, 3> result;
+    result[0] = v1[0] - v2[0];
+    result[1] = v1[1] - v2[1];
+    result[2] = v1[2] - v2[2];
+    return result;
+}
+
+double calc_rCubed(const std::array<double, 3>& v) {
+    double r2 = v[0]*v[0] + v[1]*v[1] + v[2]*v[2];
+    double rCubed = r2 * sqrt(r2);
+    return rCubed;
+}
+
 // how is this readable, look at the length man
 std::vector<std::vector<std::string>> getPlanetData() {
     // open file
@@ -59,29 +74,15 @@ std::vector<std::vector<std::string>> getPlanetData() {
     return planetData;
 }
 
-std::array<double, 3> acceleration(std::array<double, 3> rVec, double& GM) {
-    std::array<double, 3> aVec;
-
-    double rSquared = rVec[0] * rVec[0] + rVec[1] * rVec[1] + rVec[2] * rVec[2];
-    double rCubed = rSquared * sqrt(rSquared);
-
-    aVec[0] = -GM * rVec[0] / rCubed;
-    aVec[1] = -GM * rVec[1] / rCubed;
-    aVec[2] = -GM * rVec[2] / rCubed;
-
-    return aVec;
-}
-
 class Celestial {
 public:
 
-    // create publicly accessible objects
+    // create class attributes
+    std::string name;
     std::vector<std::array<double, 3>> position;
     std::vector<std::array<double, 3>> velocity;
     double mass;
     double GM;
-
-    std::string name;
 
     // constructor
     // & means reference
@@ -99,7 +100,7 @@ public:
               GM(Constants::G * inputMass)
                 
         {
-        // resize arrays
+        // resize vectors
         position.resize(nSteps);
         velocity.resize(nSteps);
 
@@ -120,17 +121,24 @@ public:
 class SolarSystem {
 public:
 
-    // set variables that should not change
+    // set unchanging constants
     const int nPlanets = 8;
     const int dt = 86400;
-    const int nSteps = 365*12;
+    const long nSteps = 365*100;
 
     // vector allows dynamic resizing and memory allocation (but slower)
     Celestial Sun;
     std::vector<Celestial> planets;
     std::vector<Celestial> centaurs;
 
+    // input data
     std::vector<std::vector<std::string>> planetData;
+
+    // allocate memory for calculations
+    std::array<double, 3> rVec;
+    std::array<double, 3> aVec;
+    double rCubed;
+    double GMrCubed;
 
     // constructor
     SolarSystem() 
@@ -152,32 +160,49 @@ public:
     }
 
     void updatePlanets(int t) {
+        // calc aVec for each planet
         for (int i = 0; i < nPlanets; i++) {
+
+            // calc gravitational pull from other planets
+            aVec = {0.0, 0.0, 0.0};
             for (int j = 0; j < nPlanets; j++) {
                 if (i != j) {
-                    std::array<double, 3> rVec;
-                    rVec[0] = planets[i].position[t-1][0] - planets[j].position[t-1][0]; 
-                    rVec[1] = planets[i].position[t-1][1] - planets[j].position[t-1][1]; 
-                    rVec[2] = planets[i].position[t-1][2] - planets[j].position[t-1][2]; 
+                    rVec = subtract3D(planets[i].position[t-1], planets[j].position[t-1]);
+                    rCubed = calc_rCubed(rVec);
+                    GMrCubed = planets[j].GM / rCubed;
 
-                    std::array<double, 3> aVec = acceleration(rVec, planets[j].GM);
-
-                    planets[i].velocity[t][0] = planets[i].velocity[t-1][0] + dt * aVec[0];
-                    planets[i].velocity[t][1] = planets[i].velocity[t-1][1] + dt * aVec[1];
-                    planets[i].velocity[t][2] = planets[i].velocity[t-1][2] + dt * aVec[2];
-
-                    planets[i].position[t][0] = planets[i].position[t-1][0] + dt * planets[i].velocity[t][0];
-                    planets[i].position[t][1] = planets[i].position[t-1][1] + dt * planets[i].velocity[t][1];
-                    planets[i].position[t][2] = planets[i].position[t-1][2] + dt * planets[i].velocity[t][2];
+                    aVec[0] -= GMrCubed * rVec[0];
+                    aVec[1] -= GMrCubed * rVec[1];
+                    aVec[2] -= GMrCubed * rVec[2];
+                    }
                 }
-        }
-    }
+
+            // calc gravitational pull from SUN
+            // Stationary sun, thus pos=rVec
+            rVec = planets[i].position[t-1];
+            rCubed = calc_rCubed(rVec);
+            GMrCubed = Sun.GM / rCubed;
+            aVec[0] -= GMrCubed * rVec[0];
+            aVec[1] -= GMrCubed * rVec[1];
+            aVec[2] -= GMrCubed * rVec[2];
+
+            planets[i].velocity[t][0] = planets[i].velocity[t-1][0] + dt * aVec[0];
+            planets[i].velocity[t][1] = planets[i].velocity[t-1][1] + dt * aVec[1];
+            planets[i].velocity[t][2] = planets[i].velocity[t-1][2] + dt * aVec[2];
+
+            planets[i].position[t][0] = planets[i].position[t-1][0] + dt * planets[i].velocity[t][0];
+            planets[i].position[t][1] = planets[i].position[t-1][1] + dt * planets[i].velocity[t][1];
+            planets[i].position[t][2] = planets[i].position[t-1][2] + dt * planets[i].velocity[t][2];
+            }   
     }
 
     void simulate() {
         std::cout << "Simulating..." << std::endl;
         for (int t=1; t < nSteps; t++) {
             updatePlanets(t);
+            if (t % 1000 == 0) {
+                std::cout << t << std::endl;
+            }
         }
     }
 
